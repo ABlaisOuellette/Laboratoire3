01b7ef9a12c1132b8a09365a10ea13410a5f93d7
Nathan Lafrenière <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2022-11-27 10:05:00 -0500

    gestion erreur

--- App1/App1/AjoutEmploye.xaml.cs
+++ App1/App1/AjoutEmploye.xaml.cs
@@ -34,11 +34,23 @@ namespace App1
 
             Employes employes = new Employes();
             {
+               
+
                 if (tbxMatricule.Text == "")
                 {
+                    
                     valide = false;
                     MatE.Visibility = Visibility.Visible;
                 }
+
+                if (tbxMatricule.Text.Length > 7)
+                {
+
+                    valide = false;
+                    MatE.Text = "Le matricule doit avoir un maximum de 7 lettre et chiffre";
+                    MatE.Visibility = Visibility.Visible;
+                }
+
 
                 if (tbxNom.Text == "")
                 {
@@ -46,9 +58,25 @@ namespace App1
                     nomE.Visibility = Visibility.Visible;
                 }
 
+                if (tbxNom.Text.Length > 30)
+                {
+
+                    valide = false;
+                    tbxNom.Text = "Le nom doit avoir un maximum de 30 lettre";
+                    tbxNom.Visibility = Visibility.Visible;
+                }
+
                 if (tbxPrenom.Text == "")
                 {
                     valide = false;
+                    prenomE.Visibility = Visibility.Visible;
+                }
+
+                if (tbxPrenom.Text.Length > 20)
+                {
+
+                    valide = false;
+                    prenomE.Text = "Le Prenom doit avoir un maximum de 20 lettre";
                     prenomE.Visibility = Visibility.Visible;
                 }
 
